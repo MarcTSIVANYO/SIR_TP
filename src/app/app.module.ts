aac1b41422441d4736a4fd05eb9914ebd8cedde1
Marc TSIVANYO <marctsivanyo@gmail.com>
2021-04-07 17:54:29 +0200

    Integration prime ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './layout/menu/menu.component';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import {PasswordModule} from 'primeng/password';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MenubarModule,
+    InputTextModule,
+    ButtonModule,
+    AppRoutingModule,
+    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
